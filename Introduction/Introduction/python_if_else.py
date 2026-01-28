c754bde25e8dce3366828c40d541e3401d818020
Aarya Jain <aaryaaa.jain22@gmail.com>
2026-01-29 00:54:03 +0530

    Add Python If-Else solution

---
+++ Introduction/Introduction/python_if_else.py
@@ -0,0 +1,13 @@
+# Problem: Python If-Else
+# Platform: HackerRank
+
+n = int(input().strip())
+
+if n % 2 != 0:
+    print("Weird")
+elif 2 <= n <= 5:
+    print("Not Weird")
+elif 6 <= n <= 20:
+    print("Weird")
+else:
+    print("Not Weird")
