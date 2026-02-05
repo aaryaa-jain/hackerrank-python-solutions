b9387f3cd6cba9b7b5fcf7a7fe9592fd49aad688
Aarya Jain <aaryaaa.jain22@gmail.com>
2026-02-05 22:42:22 +0530

    Added Set_.intersection()_Operation solution

---
+++ Sets/Set_.intersection()_Operation.py
@@ -0,0 +1,11 @@
+# Problem: Set Union Operation
+# Platform: HackerRank
+
+count_a = int(input())
+set_a = set(map(int, input().split()))
+
+count_b = int(input())
+set_b = set(map(int, input().split()))
+
+Intersection_set = set_a.intersection(set_b)
+print(len(Intersection_set))
