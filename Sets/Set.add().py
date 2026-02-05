6867701e8b7873fb5f5871655c55bbd2787685c4
Aarya Jain <aaryaaa.jain22@gmail.com>
2026-02-05 22:36:28 +0530

    Added Set.add() solution

---
+++ Sets/Set.add().py
@@ -0,0 +1,11 @@
+# Problem: Set .add()
+# Platform: HackerRank
+
+n = int(input())
+countries = set()
+
+for i in range(n):
+    country = input()
+    countries.add(country)
+
+print(len(countries))
