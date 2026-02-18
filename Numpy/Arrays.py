9a5b59d9fed12e18d3d723c46bd6f0c90f044f55
Aarya Jain <aaryaaa.jain22@gmail.com>
2026-02-18 18:13:32 +0530

    Added Arrays solution

---
+++ Numpy/Arrays.py
@@ -0,0 +1,13 @@
+# Problem: Arrays (NumPy)
+# Platform: HackerRank
+
+import numpy
+
+def arrays(arr):
+    arr.reverse()
+    return numpy.array(arr, float)
+
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
