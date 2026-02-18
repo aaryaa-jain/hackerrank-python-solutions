db2fa87006183b02abdbd41e2b0b23f9872d54cd
Aarya Jain <aaryaaa.jain22@gmail.com>
2026-02-18 18:19:00 +0530

    Added Zeros_and_Ones solution

---
+++ Numpy/Zeros_and_Ones.py
@@ -0,0 +1,9 @@
+# Problem: Zeros and Ones
+# Platform: HackerRank
+
+import numpy
+
+shape = tuple(map(int, input().split()))
+
+print(numpy.zeros(shape, dtype=int))
+print(numpy.ones(shape, dtype=int))
